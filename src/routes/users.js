056cc59d0d050c93a6ab836acbd9665b191693c2
HABIMANA Olivier <habimanaolivier6@gmail.com>
2019-09-29 13:52:23 -0700

    Chore (server): Admin can view all users - Admin can view all the users - Admin need to authonticate as an administrator [Finishes #168827078]"

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import Users from '../controllers/Users';
 import auth from '../middleware/auth';
+import admin from '../middleware/admin';
 
 const router = express.Router();
 
 router.post('/auth/signup', Users.create);
-router.get('/user/:id', Users.getOne);
+router.get('/users', [auth, admin], Users.getAll);
 router.get('/me', auth, Users.getMe);
 
 export default router;
