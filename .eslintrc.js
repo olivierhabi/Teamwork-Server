5c2fb637d50dad6907b2d8e5e445f62a1f008e8a
HABIMANA Olivier <habimanaolivier6@gmail.com>
2019-10-08 11:45:35 +0200

    Chore(server): Create database tables

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,23 @@
-export default {
-  parser: 'babel-eslint',
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: 'module'
-  },
-  extends: ['airbnb', 'prettier'],
-  plugins: ['prettier'],
-  rules: {
-    'prettier/prettier': 'warn',
-    'no-unused-vars': 'off',
-    'no-console': 'off',
-    'func-names': 'off',
-    'no-process-exit': 'off',
-    'object-shorthand': 'off',
-    strict: 'off',
-    'no-var': 'off',
-    'class-methods-use-this': 'off',
-    'lines-between-class-members': 'off',
-    'dot-notation': 'off',
-    'consistent-return': 'off',
-    'array-callback-return': 'off',
-    'no-shadow': 'off',
-    'no-undef': 'off',
-    'import/no-named-as-default': 'off',
-    'import/no-named-as-default-member': 'off',
-    'no-plusplus': 'off',
-    'import/no-cycle': 'off',
-    'import/no-unresolved': 'off',
-    'spaced-comment': 'off',
-    'no-else-return': 'off',
-    'import/order': 'off',
-    'import/newline-after-import': 'off',
-    'import/first': 'off',
-    'prefer-const': 'off'
-  }
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+    }
 };
--- src/database/index.js
+++ src/database/index.js
@@ -11,15 +11,6 @@ pool.on('connect', () => {
   console.log('Connected to database succesfully');
 });
 
-// pool.query(`SELECT * FROM team`, (err, res) => {
-//   if (err) return console.log(err);
-
-//   console.log(res);
-// });
-
-/**
- * Create tables
- */
 const createArticleTables = () => {
   const queryText = `CREATE TABLE IF NOT EXISTS
     articles(
@@ -79,9 +70,12 @@ const createUserTables = () => {
 const createCommentTables = () => {
   const queryText = `CREATE TABLE IF NOT EXISTS
   comments(
-    id UUID PRIMARY KEY,
-    created_on TIMESTAMP,
-    comment VARCHAR(128) NOT NULL
+    comment_id UUID PRIMARY KEY,
+    comment VARCHAR(128) NOT NULL,
+    author_id VARCHAR(128) NOT NULL,
+    article_id VARCHAR(128) NOT NULL,
+    article_title VARCHAR(128) NOT NULL,
+    created_on TIMESTAMP
 
   )`;
   pool
@@ -98,9 +92,9 @@ const createCommentTables = () => {
 
 const flagArticleTables = () => {
   const queryText = `CREATE TABLE IF NOT EXISTS
-  flag_article(
+  flag_articles(
     id UUID PRIMARY KEY,
-    flag TIMESTAMP,
+    flag VARCHAR(128) NOT NULL,
     article_id VARCHAR(128) NOT NULL,
     title VARCHAR(128) NOT NULL,
     article VARCHAR(128) NOT NULL,
@@ -118,25 +112,32 @@ const flagArticleTables = () => {
     });
 };
 
+const flagCommentTables = () => {
+  const queryText = `CREATE TABLE IF NOT EXISTS
+  flag_comments(
+    id UUID PRIMARY KEY,
+    flag VARCHAR(128) NOT NULL,
+    comment_id VARCHAR(128) NOT NULL,
+    comment VARCHAR(128) NOT NULL,
+    created_on TIMESTAMP
+  )`;
+  pool
+    .query(queryText)
+    .then(res => {
+      console.log(res);
+      pool.end();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
 module.exports = {
   createArticleTables,
   createUserTables,
   createCommentTables,
-  flagArticleTables
+  flagArticleTables,
+  flagCommentTables
 };
 
 require('make-runnable');
-
-// const queryText = `CREATE TABLE
-// users(
-//   id UUID PRIMARY KEY,
-//   first_name VARCHAR(128) NOT NULL,
-//   last_name VARCHAR(128) NOT NULL,
-//   email VARCHAR(128)NOT NOT NULL,
-//   password VARCHAR(128) NOT NULL,
-//   gender VARCHAR(128) NOT NULL,
-//   job_role VARCHAR(128) NOT NULL,
-//   department VARCHAR(128) NOT NULL,
-//   address VARCHAR(128) NOT NULL,
-//   is_admin BOOLEAN NOT NULL
-//   )`;
