a3acfc5257a093ef064a4dd6918b8bf43cb75a32
Olivier <habimanaolivier6@gmail.com>
2019-10-09 12:59:23 +0200

    ft (server): User can register - User can register - Generate token - Compare password - Find User by email [Finishes #169033585]

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,16 @@
 import bcrypt from 'bcryptjs';
 import '@babel/polyfill';
 import UserModel from '../models/user';
+import token from '../helpers/gentoken';
+
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
 
 class Auth {
   /**
@@ -10,24 +20,22 @@ class Auth {
    * @return {object} user token
    */
   static async getOne(req, res) {
-    const user = await UserModel.findUser(req.body.email);
-
-    if (!user)
+    const text = `SELECT * FROM users WHERE email = $1`;
+    const { rows } = await pool.query(text, [req.body.email]);
+    if (!rows[0]) {
       return res
         .status(400)
         .send({ status: 400, message: 'Invalid email or password' });
-
+    }
     const validPassword = await bcrypt.compare(
       req.body.password,
-      user.password
+      rows[0].password
     );
     if (!validPassword)
       return res
         .status(400)
         .send({ status: 400, message: 'Invalid email or password' });
-
-    const data = await UserModel.genToken(user);
-
+    const data = await token(rows[0]);
     return res
       .status(200)
       .send({ status: 200, message: 'User is successfully logged in', data });
